3b7779cd471fb9a097fa4c45f4dbb0ecc29884a3
Tommi Teräsvirta <tommi.terasvirta@digitalentconsulting.fi>
2022-03-08 15:37:13 +0200

    WebClient mono to flux

--- src/main/java/fi/digitalentconsulting/books/BookController.java
+++ src/main/java/fi/digitalentconsulting/books/BookController.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 import fi.digitalentconsulting.books.model.dto.BookTO;
 import fi.digitalentconsulting.books.service.BookService;
